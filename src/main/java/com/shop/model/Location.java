10df498e3c3cfa3969ea17495ad22cfbcdb4e95f
Nikolay Milkov <nikolay.milkov04@gmail.com>
2025-07-06 18:51:07 +0300

    location class added

---
+++ src/main/java/com/shop/model/Location.java
@@ -0,0 +1,17 @@
+package com.shop.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Location {
+    private Integer x;
+    private Integer y;
+}

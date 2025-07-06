6ad785d82023ebe337a4d4205dd4168054df48c2
Nikolay Milkov <nikolay.milkov04@gmail.com>
2025-07-06 19:02:20 +0300

    error handlers response status code added

--- src/main/java/com/shop/controllers/BaseController.java
+++ src/main/java/com/shop/controllers/BaseController.java
@@ -6,12 +6,14 @@ import com.shop.exceptions.UnauthorisedException;
 import com.shop.model.RespMessageDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseController {
 
     @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public RespMessageDTO handleBadRequest(Exception e) {
         RespMessageDTO errorDTO = new RespMessageDTO();
         errorDTO.setMessage(e.getMessage());
@@ -20,6 +22,7 @@ public class BaseController {
     }
 
     @ExceptionHandler(UnauthorisedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public RespMessageDTO handleUnauthorised(Exception e) {
         RespMessageDTO errorDTO = new RespMessageDTO();
         errorDTO.setMessage(e.getMessage());
@@ -28,6 +31,7 @@ public class BaseController {
     }
 
     @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public RespMessageDTO handleNotFound(Exception e) {
         RespMessageDTO errorDTO = new RespMessageDTO();
         errorDTO.setMessage(e.getMessage());

c066b5822a6dd8c0d02d04a0c0c90d7b8af024e0
Nikolay Milkov <nikolay.milkov04@gmail.com>
2025-07-06 18:49:16 +0300

    user service added

---
+++ src/main/java/com/shop/services/UserService.java
@@ -0,0 +1,72 @@
+package com.shop.services;
+
+import com.shop.exceptions.BadRequestException;
+import com.shop.model.users.UserEntity;
+import com.shop.model.users.dtos.UserLoginDTO;
+import com.shop.model.users.dtos.UserRegisterDTO;
+import com.shop.model.users.dtos.UserRespDTO;
+import com.shop.repositories.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    public UserRespDTO registerUser(UserRegisterDTO registerUserDTO) {
+        validateRegister(registerUserDTO);
+        if (userRepository.existsByUsername(registerUserDTO.getUsername())) {
+            throw new BadRequestException("UserEntity with that username already exists");
+        }
+        UserEntity user = modelMapper.map(registerUserDTO, UserEntity.class);
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        UserEntity userWithId = userRepository.save(user);
+        return modelMapper.map(userWithId, UserRespDTO.class);
+    }
+
+    public UserRespDTO loginUser(UserLoginDTO loginUserDTO, HttpSession session) {
+        validateUsernameAndPassword(loginUserDTO.getUsername(), loginUserDTO.getPassword());
+        UserEntity u = authenticationService.authenticate(loginUserDTO, session);
+        return modelMapper.map(u, UserRespDTO.class);
+    }
+
+    public void logoutUser(HttpSession session) {
+        session.invalidate();
+    }
+
+    public void deleteUser(HttpSession session) {
+        int id = authenticationService.getUserIdFromSession(session);
+        userRepository.deleteById(id);
+    }
+
+    private void validateUsernameAndPassword(String username, String password) {
+        if (username == null || username.isBlank()) {
+            throw new BadRequestException("Username is mandatory");
+        }
+        if (password == null || password.isBlank()) {
+            throw new BadRequestException("Password is mandatory");
+        }
+    }
+
+    private void validateRegister(UserRegisterDTO dto) {
+        validateUsernameAndPassword(dto.getUsername(), dto.getPassword());
+
+        if (dto.getConfirmPassword() == null || dto.getConfirmPassword().isBlank()) {
+            throw new BadRequestException("Confirm password is mandatory");
+        }
+        if (!dto.getPassword().equals(dto.getConfirmPassword())) {
+            throw new BadRequestException("Password and conformation password do not match");
+        }
+        if (dto.getEmail() == null || dto.getEmail().isBlank()) {
+            throw new BadRequestException("Email is mandatory");
+        }
+    }
+}

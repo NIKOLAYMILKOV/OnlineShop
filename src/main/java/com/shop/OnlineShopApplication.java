909462da39058ac280ba64a20c0045750f92263e
Nikolay Milkov <nikolay.milkov04@gmail.com>
2025-07-06 18:46:03 +0300

    configuration changes

--- src/main/java/com/shop/OnlineShopApplication.java
+++ src/main/java/com/shop/OnlineShopApplication.java
@@ -1,13 +1,44 @@
 package com.shop;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @SpringBootApplication
+@Configuration
 public class OnlineShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShopApplication.class, args);
 	}
 
+	public Connection connection() {
+		Connection connection;
+		try	{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/online_shop"
+				, "root"
+				, "2wsx#EDC");
+		} catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
